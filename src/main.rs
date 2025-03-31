9b28c2eb578bd6488638c5e36af0c9eba995554a
rg63 <rg@kando.care>
2025-03-31 16:41:59 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        //println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
-
-    //println!("Searching for {}", config.query);
-    //println!("In file {}", config.file_path);
-
-    //println!("Reading file: {}", config.file_path);
 
     // no need to return an unwrap value
     // we use a simple if let ... instead of unwrap_or_else(...)
     if let Err(e) = mini_grep::run(config) {
-        //println!("App error: {}", e);
+        eprintln!("App error: {}", e);
         process::exit(1);
     }
 }
