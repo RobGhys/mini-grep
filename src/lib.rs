3ebff8caff4a329456d807c513ff879bc700401a
rg63 <rg@kando.care>
2025-04-02 12:32:17 +0200

    improvements from ch13

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            panic!("Not enough arguments were provided. Expected 2 at least.");
-        }
+    // 'args' implements an Iterator that returns Strings
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        // copy the data
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a query string!")
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a file path!")
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {query, file_path, ignore_case})
@@ -43,15 +49,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 // -> data returned by fn search will live as long as the data passed
 // ... in the function's 'contents' &str
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
